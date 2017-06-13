355d0dbe95b43ec8ce17cfec528958049142c4c0
wingjay <wingjaysjtu@gmail.com>
2017-06-13 11:43:21 +0800

    classloader related

---
+++ app/src/main/java/com/wingjay/jayandroid/classloader/ClassLoaderActivity.java
@@ -0,0 +1,17 @@
+package com.wingjay.jayandroid.classloader;
+
+import android.os.Bundle;
+
+import com.wingjay.jayandroid.BaseActivity;
+
+/**
+ * Created by Jay on 5/22/17.
+ */
+
+public class ClassLoaderActivity extends BaseActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
