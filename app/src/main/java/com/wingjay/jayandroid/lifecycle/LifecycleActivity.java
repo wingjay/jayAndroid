a1e97608b6bc09fb0cc03f97f164fd1eb4cd3d84
冲灵 <yj142679@alibaba-inc.com>
2017-09-24 16:24:17 +0800

    taste LiveData

--- app/src/main/java/com/wingjay/jayandroid/lifecycle/LifecycleActivity.java
+++ app/src/main/java/com/wingjay/jayandroid/lifecycle/LifecycleActivity.java
@@ -1,7 +1,12 @@
 package com.wingjay.jayandroid.lifecycle;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import com.wingjay.jayandroid.BaseActivity;
+import com.wingjay.jayandroid.lifecycle.LocationLiveData.Location;
 
 /**
  * LifecycleActivity, taste google arch.lifecycle lib.
@@ -17,6 +22,14 @@ public class LifecycleActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getLifecycle().addObserver(listener);
+        //getLifecycle().addObserver(listener);
+
+        LiveData<Location> liveData = new LocationLiveData();
+        liveData.observe(this, new Observer<Location>() {
+            @Override
+            public void onChanged(@Nullable Location location) {
+                Log.e(TAG, location != null ? "onChanged: location: " + location.name : "onChanged: empty location");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/wingjay/jayandroid/lifecycle/LocationLiveData.java
@@ -0,0 +1,60 @@
+package com.wingjay.jayandroid.lifecycle;
+
+import java.util.concurrent.TimeUnit;
+
+import android.arch.lifecycle.LiveData;
+import android.util.Log;
+import com.wingjay.jayandroid.lifecycle.LocationLiveData.Location;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
+/**
+ * LocationLiveData
+ *
+ * @author wingjay
+ * @date 2017/09/24
+ */
+public class LocationLiveData extends LiveData<Location> {
+    private static final String TAG = "jaydebug";
+    private Subscription loadDataSubscription;
+    @Override
+    protected void onActive() {
+        super.onActive();
+        Log.e(TAG, "onActive LocationLiveData");
+        loadData();
+    }
+
+    @Override
+    protected void onInactive() {
+        super.onInactive();
+        Log.e(TAG, "onInactive LocationLiveData");
+        stopLoad();
+    }
+
+    private void loadData() {
+        Log.e(TAG, "loadData: START");
+        loadDataSubscription = Observable.interval(2, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Action1<Long>() {
+                @Override
+                public void call(Long aLong) {
+                    Log.e(TAG, "call: SetValue" + aLong);
+                    setValue(new Location("ShangHai" + aLong));
+                }
+            });
+    }
+
+    private void stopLoad() {
+        loadDataSubscription.unsubscribe();
+        Log.e(TAG, "loadData: STOP");
+    }
+
+    static class Location {
+        String name;
+        public Location(String name) {
+            this.name = name;
+        }
+    }
+}
