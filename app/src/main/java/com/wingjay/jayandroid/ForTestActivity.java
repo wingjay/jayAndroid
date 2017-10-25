2831440e3e45c3e01a87476481cfa7a48b911bd7
冲灵 <yj142679@alibaba-inc.com>
2017-10-25 11:35:25 +0800

    测试异步加载数据与onCreate时序

--- app/src/main/java/com/wingjay/jayandroid/ForTestActivity.java
+++ app/src/main/java/com/wingjay/jayandroid/ForTestActivity.java
@@ -1,14 +1,12 @@
 package com.wingjay.jayandroid;
 
-import java.util.concurrent.TimeUnit;
-
 import android.os.Bundle;
 import android.util.Log;
-import com.wingjay.jayandroid.autolifecycle.ActivityLifecycle;
+import android.widget.Button;
 import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
-import rx.functions.Func0;
-import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Jay on 5/10/17.
@@ -16,33 +14,57 @@ import rx.functions.Func1;
 
 public class ForTestActivity extends BaseActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    private static final String TAG = "jaydebug";
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    Observable.interval(1, TimeUnit.SECONDS)
-        .compose(this.<Long>bindUntilEvent(ActivityLifecycle.STOP))
-        .subscribe(new Action1<Long>() {
-          @Override
-          public void call(Long aLong) {
-            Log.e("jaydebug", "interval " + aLong);
-          }
-        });
+        //Observable.interval(1, TimeUnit.SECONDS)
+        //    .compose(this.<Long>bindUntilEvent(ActivityLifecycle.STOP))
+        //    .subscribe(new Action1<Long>() {
+        //      @Override
+        //      public void call(Long aLong) {
+        //        Log.e("jaydebug", "interval " + aLong);
+        //      }
+        //    });
+        //
+        //Observable loadDataObservable = Observable.defer(new Func0<Observable<Object>>() {
+        //  @Override
+        //  public Observable<Object> call() {
+        //    Log.e("jaydebug", "defer");
+        //    return Observable.just(null).map(new Func1<Object, Object>() {
+        //      @Override
+        //      public Object call(Object o) {
+        //        Log.e("jaydebug", "loadData");
+        //        return null;
+        //      }
+        //    });
+        //  }
+        //});
+        //executeWhen(loadDataObservable, ActivityLifecycle.PRE_INFLATE);
 
-    Observable loadDataObservable = Observable.defer(new Func0<Observable<Object>>() {
-      @Override
-      public Observable<Object> call() {
-        Log.e("jaydebug", "defer");
-        return Observable.just(null).map(new Func1<Object, Object>() {
-          @Override
-          public Object call(Object o) {
-            Log.e("jaydebug", "loadData");
-            return null;
-          }
-        });
-      }
-    });
-    executeWhen(loadDataObservable, ActivityLifecycle.PRE_INFLATE);
-    setContentView(R.layout.activity_test);
-  }
+        Log.e(TAG, "开始网络请求");
+        Observable.just("1")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Action1<String>() {
+                @Override
+                public void call(String s) {
+                    Log.e(TAG, "网络请求结束");
+                    button.setText(s);
+                }
+            });
+        Log.e(TAG, "setContentView，开始Inflate.");
+        try {
+            Thread.sleep(5000);
+            Log.e(TAG, "Inflate结束，耗时5秒.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setContentView(R.layout.activity_test);
+
+        Log.e(TAG, "开始findViewById");
+        button = findViewById(R.id.bt1);
+    }
 }
